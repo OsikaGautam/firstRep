ae2f311c3bad9323ecd2f6a6de344e1d3c5ff1d6
Osikagautam <osikagautam15@gmail.com>
2022-07-17 23:39:21 +0545

    sec

--- src/myproject/com/Osika.java
+++ src/myproject/com/Osika.java
@@ -3,7 +3,7 @@ package myproject.com;
 public class Osika {
 
 	public static void main(String[] args) {
-	System.out.println("my name is osika");	// TODO Auto-generated method stub
+	System.out.println("my name is osika gautam ");	// TODO Auto-generated method stub
 
 	}
 
